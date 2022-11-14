6213b6159863c098d0e6073485fc7cde98fa9bbd
mddas <mddas>
2022-11-13 23:13:01 -0800

    unhide

--- Admin/html_to_pdf.py
+++ Admin/html_to_pdf.py
@@ -4,17 +4,15 @@
  
 from django.http import HttpResponse
 from django.template.loader import get_template
- 
-
- 
+import io
 from xhtml2pdf import pisa  
 # #difine render_to_pdf() function
  
 def render_to_pdf(html):
-    result = BytesIO() 
+    result = io.BytesIO() 
      #This part will create the pdf.
-    pdf = pisa.pisaDocument(BytesIO(html.encode("utf-8")), result)
+    pdf = pisa.pisaDocument(io.BytesIO(html.encode("utf-8")), result)
     if not pdf.err:
-         return result.getvalue()
-         #return HttpResponse(result.getvalue(), content_type='application/pdf')
+         #return result.getvalue()
+         return HttpResponse(result.getvalue(), content_type='application/pdf')
     return None
