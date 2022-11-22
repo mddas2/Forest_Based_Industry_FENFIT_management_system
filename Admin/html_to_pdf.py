aae95f43f57c2b880deded7e656fab5c4d7e7ea9
mddas <mddas>
2022-11-22 02:31:25 -0800

    nothing

--- Admin/html_to_pdf.py
+++ Admin/html_to_pdf.py
@@ -28,6 +28,7 @@ def report(request):
     pdfmetrics.registerFont(TTFont("Preeti", settings.BASE_URL+static("/assets/fonts/Preeti-Font.ttf")))
     pdfmetrics.registerFont(TTFont("Preeti-Bold", settings.BASE_URL+static("/assets/fonts/Preeti-Bold.ttf")))
     pdfmetrics.registerFont(TTFont("Times", settings.BASE_URL+static("/assets/fonts/Times.ttf")))
+    pdfmetrics.registerFont(TTFont("gargi", settings.BASE_URL+static("/assets/fonts/gargi.ttf")))
     
     # Start writing the POF here
     p.setFillColorRGB(0, 0,0)
@@ -47,14 +48,27 @@ def report(request):
     p.setFont("Preeti-Bold", 22, leading=None)
     p.drawString(200,550,"laifo M ;b:otf ;DaGwdf . ")#bisaya 550
 
-    p.setFont("Preeti", 22, leading=None)
-    p.drawString(100,505,"""jg k}bfjf/df cfwfl/t ;Dk'0f{ pBf]u Joj;foLx?sf] xslxt ;+/If0f / """) 
-    p.drawString(25,480,"""k}/jL ug]{ o; lhNnf ;+3sf] ljwfgsf] clwgdf /lx gLlt, sfo{s|d tyf lg0f{ox?""") 
-    p.drawString(25,455,"""k"0f{?kdf kfngf ug]{' kg]{ 5 . """) 
+    # p.setFont("Preeti", 22, leading=None)
+    # p.drawString(25,505,""";~rfns . k|f]k|fO6/ &gt;L ============= ;Mldn ÷ e]lgo/ tyf KnfO{p8 ÷ """) 
+    # p.drawString(25,480,"""==============n] pBf]u btf{ k|df0fkq ;fy pBf]u ;&#39;lrs[t ug{sf nflu l;kmfl/;""") 
+    # p.drawString(25,455,""" dfu ul/ ===============lhNnf ;+3 . j:t&#39;ut ;+3 dfkm{t o; sfof{nodf lgj]bg """) 
+    # p.drawString(25,430,"""lbg&#39; ePsf]df pBf]u btf{ k|df0fkqsf] cfwf/ / =================k|b]z""") 
+    # p.drawString(25,405,"""dxf;+3 . j:t&#39;ut ;+3sf] l;kmfl/zsf cfwf/df df jg lgodfjnL, @)&amp;( sf] lgod !#! sf]""") 
+    # p.drawString(25,380,"""k}/jL ug]{ o; lhNnf ;+3sf] ljwfgsf] clwgdf /lx gLlt, sfo{s|d tyf lg0f{ox?""") 
+    # p.drawString(25,355,"""pklgod -@_ adf]lhd ;~rfns . k|f]k|fO6/ &gt;L =============""")
 
-    p.drawString(350,410,"""cWoIf M """) 
-    p.drawString(350,380,"""b:tvt M""") 
-    p.drawImage(settings.BASE_URL+static("/assets/images/signature.jpeg"),410,350,80,60,mask='auto') 
+    p.setFont("gargi", 22, leading=None)
+    p.drawString(25,505,""" """) 
+    p.drawString(25,480,""" """) 
+    p.drawString(25,455,""" """) 
+    p.drawString(25,430,""" """) 
+    p.drawString(25,405,""" """) 
+    p.drawString(25,380,""" """) 
+    p.drawString(25,355,""" """)
+
+    p.drawString(350,210,"""cWoIf M """) 
+    p.drawString(350,180,"""b:tvt M""") 
+    p.drawImage(settings.BASE_URL+static("/assets/images/signature.jpeg"),410,180,80,60,mask='auto') 
 
 
 
