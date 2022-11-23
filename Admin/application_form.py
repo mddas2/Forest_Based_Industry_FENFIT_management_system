c8a8bd0cda76e19c4413a7b27de17cb2cc343e17
mddas <mddas>
2022-11-23 00:13:54 -0800

    account data and central data completed

--- Admin/application_form.py
+++ Admin/application_form.py
@@ -492,8 +492,10 @@ def AllApplication(request, pk=None, approved_pending_cancelled=None):#all appli
         all_data = ApplicationForm.objects.filter(dsc__isnull=False,dsc=request.user.get_dsc_Role(),user__state_name__contains=request.user.state_name).order_by('-updated_at')  
     elif request.user.role == CustomUser.PRIVATE:
         all_data = ApplicationForm.objects.filter(dsc__isnull=False,dsc=request.user.get_dsc_Role(),user__union_name__contains=request.user.email).order_by('-updated_at') 
-    elif request.user.role == CustomUser.CENTRAL:
+    elif request.user.role == CustomUser.CENTRAL and request.user.groups.first().name == 'accountant':
         all_data = ApplicationForm.objects.filter(dsc__isnull=False,dsc=request.user.get_dsc_Role()).order_by('-updated_at') 
+    elif request.user.role == CustomUser.CENTRAL:
+        all_data = ApplicationForm.objects.filter(dsc__isnull=False,dsc=request.user.get_dsc_Role(),is_payment=True).order_by('-updated_at') 
     else:
         all_data = None         
 
