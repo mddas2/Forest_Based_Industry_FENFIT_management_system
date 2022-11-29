f7536d1e0ecee724b6d7861d7d7e51dbd233c826
mddas <mddas>
2022-11-29 08:00:54 -0800

    report url fixed

--- report/views.py
+++ report/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+import requests
+from nepallocation.models import States,Districts,Municipality
+from account.models import CustomUser
+from Admin.models import ApplicationForm,UserApplicationDetail,UserApplicationPayment
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.core import serializers
+from django.core.serializers import json
 
-# Create your views here.
+def ApplicationFormReport(request):
+     formsobj = ApplicationForm.objects.all()
+
+def MembershipReport(request):
+     member = CustomUser.objects.all()
+
