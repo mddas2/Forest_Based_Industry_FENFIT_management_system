bc035b4aa7eb07a4821780512657534e71a8a574
Devraj <devraj.sah310@gmail.com>
2022-11-29 22:34:19 +0545

    devraj

--- report/views.py
+++ report/views.py
@@ -6,9 +6,11 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 from django.core import serializers
 from django.core.serializers import json
+from django.shortcuts import render
 
 def ApplicationFormReport(request):
      formsobj = ApplicationForm.objects.all()
+     return render(request,'report/application-lists.html')     
 
 def MembershipReport(request):
      member = CustomUser.objects.all()
