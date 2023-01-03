27eb020e5e8a367243e800352285cdf7faa4a40d
mddas <mddas>
2023-01-02 23:20:06 -0800

    report pagination

--- report/views.py
+++ report/views.py
@@ -19,17 +19,51 @@ from django.shortcuts import redirect
 from Admin.decorators import customized_user_passes_test,is_admin_role,is_central_role
 from django.contrib.auth.decorators import login_required
 from rest_framework.renderers import JSONRenderer
+from django.core.paginator import Paginator
 
 @login_required(login_url=settings.LOGIN_URL)
 @customized_user_passes_test(is_central_role)
 def ApplicationFormReport(request):
      approved_admin =  CustomUser.objects.filter(role = CustomUser.CENTRAL,groups__name__contains = 'ceo').first()
-     slug = "सुचिकरण सिफारिश Report"
+     slug = "सुचिकरण सिफारिश रिपोर्ट"
      report_type = 'application'
-     formsobj = ApplicationForm.objects.filter(dsc__isnull=False).order_by('-id')
+     all_data = ApplicationForm.objects.filter(dsc__isnull=False).order_by('-id')
+
+     page_number = request.GET.get('page')
+     page_type = request.GET.get('type')
+     range = request.GET.get('range')
+     next = 2
+     prev = 1
+    
+     try:
+         try:
+             paginate_obj = Paginator(all_data, int(range)) #creating pagiting (only 5 data)
+         except:
+             paginate_obj = Paginator(all_data, 2)
+    
+         try:
+             page_number = int(page_number)
+             if page_type == "next":
+                 next = page_number+1
+             elif page_number == 'prev':
+                 prev = page_number-1
+         except:
+             page_number = 2
+         try:
+             all_data = paginate_obj.get_page(page_number)  # returns the desired page object
+         except:
+             # if page_number is not an integer then assign the first page
+             all_data = paginate_obj.page(1)
+     except:
+         range = 2
+         all_data = None
+    
      # return HttpResponse(formsobj.first().dsc)
      data={
-        'all_data' : formsobj,
+        'all_data' : all_data,
+        'next' : next,
+        'prev' : prev,
+        'range' : range,
         'export_link_name' : reverse("ExportProduct"),
         'report_type':report_type,
         'slug1' : slug,
@@ -40,12 +74,44 @@ def ApplicationFormReport(request):
 @login_required(login_url=settings.LOGIN_URL)
 @customized_user_passes_test(is_central_role)
 def MembershipReport(request):
-     slug = "सदस्यता Report"
+     slug = "सदस्यता रिपोर्ट"
      report_type = 'membership'
-     formsobj = ApplicationForm.objects.filter(user__is_applyForVerified__contains=1).order_by('-id')
+     all_data = ApplicationForm.objects.filter(user__is_applyForVerified__contains=1).order_by('-id')
+
+     page_number = request.GET.get('page')
+     page_type = request.GET.get('type')
+     range = request.GET.get('range')
+     next = 2
+     prev = 1
+    
+     try:
+         try:
+             paginate_obj = Paginator(all_data, int(range)) #creating pagiting (only 5 data)
+         except:
+             paginate_obj = Paginator(all_data, 2)
+    
+         try:
+             page_number = int(page_number)
+             if page_type == "next":
+                 next = page_number+1
+             elif page_number == 'prev':
+                 prev = page_number-1
+         except:
+             page_number = 2
+         try:
+             all_data = paginate_obj.get_page(page_number)  # returns the desired page object
+         except:
+             # if page_number is not an integer then assign the first page
+             all_data = paginate_obj.page(1)
+     except:
+         range = 2
+         all_data = None
      # return HttpResponse(formsobj.first().dsc)
      data={
-        'all_data' : formsobj,
+        'all_data' : all_data,
+        'next' : next,
+        'prev' : prev,
+        'range' : range,
         'export_link_name' : reverse("ExportProduct"),
         'report_type':report_type,
         'slug1' : slug
