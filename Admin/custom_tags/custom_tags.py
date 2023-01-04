eb86baee4366adc4db91f381a342f20511a6b787
mddas <mddas>
2023-01-04 01:21:45 -0800

    2 notification

--- Admin/custom_tags/custom_tags.py
+++ Admin/custom_tags/custom_tags.py
@@ -97,16 +97,16 @@ def getTotamMember(request):
 def getTotalApplication(request):
     try:
         if request.user.role == CustomUser.DISTRICT:
-            all_data = ApplicationForm.objects.filter(dsc__isnull=False,dsc=request.user.get_dsc_Role(),user__district_name__contains=request.user.district_name).order_by('-updated_at')[:5]  
+            all_data = ApplicationForm.objects.filter(dsc__isnull=False,dsc=request.user.get_dsc_Role(),user__district_name__contains=request.user.district_name).order_by('-updated_at')[:1]  
         elif request.user.role == CustomUser.STATE:
-            all_data = ApplicationForm.objects.filter(dsc__isnull=False,dsc=request.user.get_dsc_Role(),user__state_name__contains=request.user.state_name).order_by('-updated_at')[:5]   
+            all_data = ApplicationForm.objects.filter(dsc__isnull=False,dsc=request.user.get_dsc_Role(),user__state_name__contains=request.user.state_name).order_by('-updated_at')[:1]   
         elif request.user.role == CustomUser.PRIVATE:
-            all_data = ApplicationForm.objects.filter(dsc__isnull=False,dsc=request.user.get_dsc_Role(),user__union_name__contains=request.user.email).order_by('-updated_at')[:5] 
+            all_data = ApplicationForm.objects.filter(dsc__isnull=False,dsc=request.user.get_dsc_Role(),user__union_name__contains=request.user.email).order_by('-updated_at')[:1] 
         elif request.user.role == CustomUser.CENTRAL:
             if request.user.get_dsc_Role() == 'central_accountant':
-                all_data = ApplicationForm.objects.filter(Q(dsc=request.user.get_dsc_Role()) | Q(dsc='central_admin'),dsc__isnull=False,).order_by('-updated_at')[:5] #admin can view both data from accountant and self
+                all_data = ApplicationForm.objects.filter(Q(dsc=request.user.get_dsc_Role()) | Q(dsc='central_admin'),dsc__isnull=False,).order_by('-updated_at')[:1] #admin can view both data from accountant and self
             else:
-                all_data = ApplicationForm.objects.filter(dsc__isnull=False,dsc=request.user.get_dsc_Role()).order_by('-updated_at')[:5]
+                all_data = ApplicationForm.objects.filter(dsc__isnull=False,dsc=request.user.get_dsc_Role()).order_by('-updated_at')[:1]
         else:
             all_data = None 
         return all_data
